98dc340bcb26e3f637e938fd977689191c2b1d61
Andrew Ushakov <52571870+Di-Strix@users.noreply.github.com>
2022-06-18 18:14:30 +0200

    modify functions for ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,11 @@
+import { Answer, QuestionType, Quiz, QuizId } from '@di-strix/quizee-types';
+
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
 export const getQuizeesList = functions.https.onCall(async (_, context) => {
-  // {
-  //     caption,
-  //     img,
-  //     questionsCount,
-  //     quizeeId
-  // }
-
   if (context.app == undefined) {
     throw new functions.https.HttpsError(
       'failed-precondition',
@@ -18,7 +13,7 @@ export const getQuizeesList = functions.https.onCall(async (_, context) => {
     );
   }
 
-  let dbData = {};
+  let dbData: { [key: QuizId]: Quiz } = {};
 
   await admin
     .database()
@@ -27,16 +22,16 @@ export const getQuizeesList = functions.https.onCall(async (_, context) => {
     .once('value', (snapshot) => (dbData = snapshot.val()));
 
   const responseData = Object.keys(dbData).map((quizeeId) => ({
-    caption: dbData[quizeeId].content.caption,
-    img: dbData[quizeeId].content.img || '',
-    questionsCount: dbData[quizeeId].content.questions.length,
+    caption: dbData[quizeeId].info.caption,
+    img: dbData[quizeeId].info.img,
+    questionsCount: dbData[quizeeId].info.questionsCount,
     id: quizeeId,
   }));
 
   return responseData;
 });
 
-export const checkAnswers = functions.https.onCall(async (data, context) => {
+export const checkAnswers = functions.https.onCall(async (data: { answers: Answer[]; quizId: QuizId }, context) => {
   if (context.app == undefined) {
     throw new functions.https.HttpsError(
       'failed-precondition',
@@ -45,47 +40,59 @@ export const checkAnswers = functions.https.onCall(async (data, context) => {
   }
 
   const userAnswers = data.answers;
-  let rightAnswers = {};
+  let quiz!: Quiz;
   await admin
     .database()
-    .ref('quizees/' + data.quizeeId + '/answers')
-    .once('value', (snapshot) => (rightAnswers = snapshot.val()));
+    .ref('quizees/' + data.quizId)
+    .once('value', (snapshot) => (quiz = snapshot.val()));
 
-  const checkCases = {
-    array: (answerObject, userAnswers) => {
-      const factor = 1 / answerObject.answer.length;
+  const checkCases: {
+    [Type in QuestionType]: (questionPair: Answer, userAnswers: Answer['answer']) => number;
+  } = {
+    SEVERAL_TRUE: ({ answer: answers }, userAnswers) => {
+      const factor = 1 / answers.length;
       const result = userAnswers.reduce((acc, val) => {
-        if (answerObject.answer.includes(val)) acc += factor;
+        if (answers.includes(val)) acc += factor;
         else acc -= factor;
         return acc;
       }, 0);
 
       return result < 0 ? 0 : result;
     },
-    number: (rightAnswer, userAnswer) => rightAnswer.answer === userAnswer,
-    string: (answerObject, userAnswer) => {
-      const config = {
-        equalCase: false,
-        ...answerObject.config,
-      };
-      if (!config.equalCase) {
-        userAnswer = userAnswer.toUpperCase();
-        answerObject.answer = answerObject.answer.toUpperCase();
+    ONE_TRUE: ({ answer: answers }, userAnswer) => Number(answers[0] === userAnswer[0]),
+    WRITE_ANSWER: (answer, userAnswer) => {
+      const map = <T>(mappers: Array<(arg: T) => T>, startValue: T) =>
+        mappers.reduce((acc, mapper) => mapper(acc), startValue);
+
+      const userAnswerMappers: Array<(userAnswer: string) => string> = [];
+      const actualAnswerMappers: Array<(actualAnswer: string) => string> = [];
+
+      if (!answer.config.equalCase) {
+        const upperCaser = (str: string) => str.toUpperCase();
+        userAnswerMappers.push(upperCaser);
+        actualAnswerMappers.push(upperCaser);
       }
 
-      return answerObject.answer == userAnswer;
+      return Number(map(actualAnswerMappers, answer.answer[0]) === map(userAnswerMappers, userAnswer[0]));
     },
   };
 
-  const getType = (v) => (Array.isArray(v) ? 'array' : typeof v);
+  const correctAnswers = quiz.answers;
 
-  if (userAnswers.length != rightAnswers.length) throw new Error("Answers count don't equal");
-  const factor = 100 / rightAnswers.length;
-  const result = rightAnswers.reduce((acc, value, index) => {
-    console.log(index, getType(value.answer), value);
-    const handler = checkCases[getType(value.answer)];
+  if (userAnswers.length != correctAnswers.length) throw new Error("Answers count don't equal");
+  const factor = 100 / correctAnswers.length;
+  const result = correctAnswers.reduce((acc, actualAnswer, index) => {
+    const questionType = quiz.questions.find((question) => question.id === actualAnswer.answerTo)?.type;
 
-    acc += factor * handler(value, userAnswers[index]);
+    if (!questionType) {
+      console.log('Invalid question type');
+      return acc;
+    }
+
+    console.log(index, questionType, actualAnswer);
+    const handler = checkCases[questionType];
+
+    acc += factor * handler(actualAnswer, userAnswers[index].answer);
     acc = parseFloat(acc.toFixed(1));
     return acc;
   }, 0);
